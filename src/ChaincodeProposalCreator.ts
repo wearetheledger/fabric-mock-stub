70d5081c6a25173ef7e3e2a3a9bd6a66fc8c6a5c
Kevin Osullivan <kevin.osullivan@sita.aero>
2019-01-08 18:40:12 +0000

    Added code to add a toBuffer() function onto the ChaincodeProposalCreator

--- src/ChaincodeProposalCreator.ts
+++ src/ChaincodeProposalCreator.ts
@@ -5,20 +5,22 @@ import { SerializedIdentity } from 'fabric-shim';
  */
 export class ChaincodeProposalCreator implements SerializedIdentity {
     // tslint:disable-next-line:variable-name
-    id_bytes: Buffer;
+    id_bytes: any;
 
     [fieldName: string]: any;
     mspid: string;
 
     constructor(private mspId: string, private signingId: string) {
-        this.id_bytes = Buffer.from(signingId);
+       this.id_bytes = Buffer.from(signingId);
+       // fabric-shim 1.3 makes a call to  toBuffer() in ClientIdentity constructor. We need to add this function to the id_bytes.
+       this.id_bytes.toBuffer = function() { return this; };
     }
 
     getMspid(): string {
         return this.mspId;
     }
 
-    getIdBytes(): Buffer {
+    getIdBytes(): any {
         return this.id_bytes;
     }
 }
